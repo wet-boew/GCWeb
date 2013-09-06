77d2700ec453476a6fd5710e95e6a7559dabbe86
Masterbee <mario@beelabs.ca>
2013-09-06 15:35:44 -0400

    Initial scaffolding for GCWeb Theme

---
+++ bower.js
@@ -0,0 +1,30 @@
+{
+  "name": "GCWeb",
+  "version": "v1.0-a1-development",
+  "homepage": "Web Experience Toolkit (WET): GCWeb Theme",
+  "authors": [
+    "WET-BOEW Team"
+  ],
+  "description": "Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)",
+  "keywords": [
+    "i18n",
+    "a11y",
+    "wet-boew",
+    "bootstrap"
+  ],
+  "license": "MIT",
+  "private": true,
+  "ignore": [
+    "**/.*",
+    "node_modules",
+    "bower_components",
+    "test",
+    "tests"
+  ],
+  "dependencies": {
+    "wet-boew": "git@github.com:wet-boew/wet-boew.git#v4.0"
+  },
+  "resolutions": {
+    "jquery": ">= 1.9.0"
+  }
+}
