64353b1ca0fc66bca318f94c745c8d9cbf0374d3
Nick Schonning <nschonni@gmail.com>
2013-09-09 15:58:36 -0400

    Initial check-in of dist folder

---
+++ dist/js/i18n/pt-BR.js
@@ -0,0 +1,207 @@
+/*!
+ *
+ * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
+ * wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
+ *
+ * Version: @wet-boew-build.version@
+ *
+ */
+/*
+----- Portuguese (Brazilian) dictionary (il8n) ---
+ */
+(function() {
+	"use strict";
+	var _pe = window.pe || {
+		fn: {}
+	};
+	_pe.dic.ind = {
+		'%lang-code': 'pt-BR',
+		'%lang-eng': '@%lang-eng@',
+		'%lang-fra': 'portugais (brésilien)',
+		'%lang-native': 'Português do Brasil',
+		'%all': 'Tudo',
+		'%home': 'Página inicial',
+		'%main-page': 'Página principal',
+		'%top-of-page': 'Início da Página',
+		'%you-are-in': 'Você está no/a:',
+		'%welcome-to': 'Bem-vindo/a à/ao:',
+		'%loading': 'carregando ...',
+		'%processing': 'processando ...',
+		'%search': 'Buscar',
+		'%search-for-terms': 'Buscar por palavra(s):',
+		'%no-match-found': 'Nenhum resultado encontrado',
+		'%matches-found': {
+			'mixin': '[MIXIN] Resulatdo(s) Encontrado(s)'
+		},
+		'%menu': 'Menu',
+		'%settings': 'Configurações',
+		'%languages': 'Idiomas',
+		'%about': 'Sobre',
+		'%current': '(atual)',
+		'%hide': 'Ocultar',
+		'%error': 'Erro',
+		'%colon': ':',
+		'%hyphen': ' - ',
+		'%start': 'Iniciar',
+		'%stop': 'Parar',
+		'%back': 'Voltar',
+		'%new-window': ' (Abre em uma nova janela)',
+		'%minute-ago': 'há um minuto',
+		'%couple-of-minutes': 'há dois minutos',
+		'%minutes-ago': {
+			'mixin': 'há [MIXIN] minutos'
+		},
+		'%hour-ago': 'há uma hora',
+		'%hours-ago': {
+			'mixin': 'há [MIXIN] horas'
+		},
+		'%days-ago': {
+			'mixin': 'há [MIXIN] dias'
+		},
+		'%yesterday': 'ontem',
+
+		'%next': 'Próximo/a',
+		'%previous': 'Anterior',
+		'%first': 'Primeiro/a',
+		'%last': 'Último/a',
+
+		/* Archived Web page template */
+		'%archived-page': 'Está página foi arquiva na internet.',
+		/* Menu bar */
+		'%sub-menu-help': '(abrir o sub-menu com o botão ENTER e fechá-lo com o botão ESC)',
+		/* Tabbed interface */
+		'%tab-rotation': {
+			'disable': 'Interromper a rotação das abas',
+			'enable': 'Iniciar a rotação das abas'
+		},
+		'%tab-list': 'Lista de Abas',
+		'%tab-panel-end-1': 'Final deste painel da guia.',
+		'%tab-panel-end-2': 'Voltar para a lista de guias',
+		'%tab-panel-end-3': 'ou continuar com o restante da página.',
+		/* Multimedia player */
+		'%play': 'Executar',
+		'%pause': 'Pausar',
+		'%open': 'Abrir',
+		'%close': 'Fechar',
+		'%rewind': 'Voltar',
+		'%fast-forward': 'Avançar',
+		'%mute': {
+			'enable': 'Ativar o modo silencioso',
+			'disable': 'Desativar o modo silencioso'
+		},
+		'%closed-caption': {
+			'disable': 'Ocultar a legenda',
+			'enable': 'Mostrar a legenda'
+		},
+		'%closed-caption-error': 'Ocorreu um erro no carregamento da legenda',
+		'%audio-description': {
+			'enable': 'Ativar a descrição de áudio',
+			'disable': 'Desativar a descrição de áudio'
+		},
+		'%progress-bar': 'usar os botões de seta ESQUERDA e DIREITA para voltar ou avançar a execução da mídia',
+		'%no-video': 'Seu navegador não parece ter capacidade para reproduzir este vídeo. Favor baixar o vídeo abaixo.',
+		'%position': 'Posição atual:',
+		'%percentage': 'Porcentagem de reprodução:',
+		'%duration': 'Tempo total:',
+		'%buffered': 'Armazenado em área de memória temporária',
+		/* Share widget */
+		'%favourite': 'Favoritos',
+		'%email': 'Correio eletrônico',
+		'%share-text': 'Compartilhar esta página',
+		'%share-hint': ' com {s} ',
+		'%share-email-subject': 'Página interessante',
+		'%share-email-body': 'Espero que esta página seja do seu interesse:\n{t} ({u})',
+		'%share-fav-title': '(salvar esta página)',
+		'%share-manual': 'Favor fechar este diálogo e teclar Ctrl+D para adicionar esta página aos seus favoritos.',
+		'%share-showall': 'Mostrar tudo ({n})',
+		'%share-showall-title': 'Todas as páginas salvas em favoritos',
+		'%share-disclaimer' : 'Nenhum endosso, expresso ou implícito, é concedido a quaisquer produtos ou serviços',
+		/* Form validation */
+		'%form-not-submitted': 'O formulário não pode ser submetido porque ',
+		'%errors-found': ' foram encontrados erros.',
+		'%error-found': ' foi encontrado um erro.',
+		/* Date picker */
+		'%datepicker-hide': 'Ocultar o calendário',
+		'%datepicker-show': 'Selecionar uma data de um calendário para o campo:',
+		'%datepicker-selected': 'Selecionado',
+		/* Calendar */
+		'%calendar-weekDayNames': ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'],
+		'%calendar-monthNames': ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+		'%calendar': 'Calendário',
+		'%calendar-currentDay': '(hoje)',
+		'%calendar-goToLink': 'Ir para o<span class="wb-invisible"> mês do ano</span>',
+		'%calendar-goToTitle': 'Ir para o mês do ano',
+		'%calendar-goToMonth': 'Mês:',
+		'%calendar-goToYear': 'Ano:',
+		'%calendar-goToButton': 'Ir',
+		'%calendar-cancelButton': 'Cancelar',
+		'%calendar-previousMonth': 'Mês anterior: ',
+		'%calendar-nextMonth': 'Próximo mês: ',
+		/* Slideout */
+		'%show-toc': 'Exibir',
+		'%show-text': 'Ver o índice',
+		'%hide-text': 'Ocultar o índice',
+		'%table-contents': 'Índice',
+		/* Lightbox */
+		'%lb-current': 'Ítem {current} de {total}',
+		'%lb-next': 'Próximo ítem',
+		'%lb-prev': 'Ítem anterior',
+		'%lb-xhr-error': 'O carragemento deste conteúdo falhou.',
+		'%lb-img-error': 'O carregamento desta imagem falhou.',
+		'%lb-slideshow': 'slideshow',
+		/* jQuery Mobile */
+		'%jqm-expand': 'clicar para ver o conteúdo',
+		'%jqm-collapse': 'clicar para ocultar o conteúdo',
+		'%jqm-clear-search': 'Apagar o texto da pesquisa',
+		'%jqm-filter': 'Filtrar os ítens',
+		'%jqm-tbl-col-toggle': 'Colunas',
+		/* Charts widget */
+		'%table-mention': 'Tabela',
+		'%table-following': 'Gráfico. Mais detalhes na tabela a seguir.',
+		/* Session timeout */
+		'%st-timeout-msg': 'Sua sessão está prestes a expirar. Você tem #expireTime# para clicar o botão "OK" abaixo e prolongar sua sessão.',
+		'%st-msgbox-title': 'Aviso de término da sessão',
+		'%st-already-timeout-msg': 'Desculpe, sua sessão já expirou. Por favor, faça novo login.',
+		/* Toggle details */
+		'%td-toggle': 'Alternar todos',
+		'%td-open': 'Expandir todos',
+		'%td-close': 'Ocultar tudo',
+		'%td-ttl-open': 'Expandir todas as seções do conteúdo',
+		'%td-ttl-close': 'Ocultar todas as seções do conteúdo',
+		/* Table enhancement */
+		'%sSortAscending': 'ativar classificação ascendente',
+		'%sSortDescending': 'ativar para descer tipo',
+		'%sEmptyTable': 'Não há dados disponíveis na tabela',
+		'%sInfo': 'Exibindo _START_ a _END_ de _TOTAL_ entradas',
+		'%sInfoEmpty': 'Exibindo 0 a 0 de 0 entradas',
+		'%sInfoFiltered': '(filtrado de _MAX_ entradas no total)',
+		'%sInfoThousands': '&#160;',
+		'%sLengthMenu': 'Mostrar _MENU_ entradas',
+		/* Geomap */
+		'%geo-mapcontrol': 'Controle de mapa',
+		'%geo-zoomin': 'Ampliar',
+		'%geo-zoomout': 'Reduzir',
+		'%geo-zoomworld': 'Reduzir até o tamanho do mapa',
+		'%geo-zoomfeature': 'Ampliar elemento',
+		'%geo-scaleline': 'escala do mapa',
+		'%geo-mouseposition': 'Latitude e longitude do cursor do mouse',
+		'%geo-ariamap': 'Objeto do mapa. As descrições das características do mapa estão na tabela abaixo.',
+		'%geo-accessibilize': '<strong>Usuários de teclado:</strong> Quando o mapa estiver em foco, use as teclas de seta para mover o mapa e as teclas de mais e de menos para ampliar e reduzir, respectivamente. As teclas de seta não funcionarão quando a imagem estiver reduzida ao tamanho do mapa.',
+		'%geo-accessibilizetitle': 'Instruções: Como navegar pelo mapa',
+		'%geo-togglelayer': 'Alternar a exibição da camada',
+		'%geo-hiddenlayer': 'Esta camada está oculta.',
+		'%geo-basemapurl': 'http://geoappext.nrcan.gc.ca/arcgis/rest/services/BaseMaps/CBMT_CBCT_GEOM_3978/MapServer/WMTS/tile/1.0.0/BaseMaps_CBMT3978/{Style}/{TileMatrixSet}/{TileMatrix}/{TileRow}/{TileCol}.jpg',
+		'%geo-basemaptitle': 'BaseMaps_CBMT3978',
+		'%geo-basemapurltxt': 'http://geoappext.nrcan.gc.ca/arcgis/rest/services/BaseMaps/CBMT_TXT_3978/MapServer/WMTS/tile/1.0.0/BaseMaps_CBMT3978/{Style}/{TileMatrixSet}/{TileMatrix}/{TileRow}/{TileCol}.jpg',
+		'%geo-attributionlink': 'http://geogratis.gc.ca/geogratis/CBM_CBC?lang=en',
+		'%geo-attributiontitle': 'GeoGratis - Canadá mapa base (Inglês ou francês)',
+		'%geo-select': 'Selecionar',
+		'%geo-labelselect': 'Clicar para selecionar o elemento no mapa',
+		/* Disable/enable PE */
+		'%pe-disable': 'Versão HTML simplificada',
+		'%pe-enable': 'Versão padrão'
+	};
+	_pe.document.trigger('languageloaded');
+	window.pe = _pe;
+	return _pe;
+}());
