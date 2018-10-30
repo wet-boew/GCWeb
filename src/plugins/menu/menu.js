7b56253ef9eab3c005149a0d52c50089839c5d01
Pierre Dubois <pierre.dubois@servicecanada.gc.ca>
2018-10-30 16:00:35 -0400

    V2 - Menu keyboard navigation fix

--- src/plugins/menu/menu.js
+++ src/plugins/menu/menu.js
@@ -9,8 +9,8 @@ var globalTimeoutOn = {},
 	globalTimeoutOnTry2,
 	globalTimeoutOffTry2,
 	hoverDelay = 350,
-	docRootNode = document.getRootNode().children[ 0 ],
-	justOpened;
+	justOpened,
+	isMobileMode = document.querySelector( "html" ).className.indexOf( "smallview" ) !== -1; // Mobile vs Desktop
 
 function OpenMenu( elm ) {
 
@@ -20,7 +20,7 @@ function OpenMenu( elm ) {
 	var menuOpen = parentMenu.querySelector( "[aria-haspopup][aria-expanded=true]:not(.gcweb-v2-not-tabble-persist)" );
 
 	// Only close other menu in tablet and desktop mode
-	if ( menuOpen && docRootNode.className.indexOf( "smallview" ) === -1 ) {
+	if ( menuOpen && !isMobileMode ) {
 		CloseMenu( menuOpen, true );
 	}
 
@@ -82,7 +82,7 @@ function CloseMenuWithDelay( elm ) {
 wb.doc.on( "mouseenter", "[aria-haspopup]", function( event ) {
 
 	// There is no "mouseenter" in mobile
-	if ( docRootNode.className.indexOf( "smallview" ) === -1 ) {
+	if ( !isMobileMode ) {
 		OpenMenuWithDelay( event.currentTarget );
 	}
 } );
@@ -91,7 +91,7 @@ wb.doc.on( "mouseenter", "[aria-haspopup]", function( event ) {
 wb.doc.on( "focusin", "[aria-haspopup]", function( event ) {
 
 	// Don't open the submenu
-	if ( docRootNode.className.indexOf( "smallview" ) !== -1 ) {
+	if ( isMobileMode ) {
 		return;
 	}
 
@@ -113,7 +113,7 @@ wb.doc.on( "mouseenter focusin", "[aria-haspopup] + [role=menu]", function( even
 	}
 
 	// There is no "mouseenter" in mobile
-	if ( docRootNode.className.indexOf( "smallview" ) !== -1 ) {
+	if ( isMobileMode ) {
 		return;
 	}
 	clearTimeout( globalTimeoutOnTry2 );
@@ -124,7 +124,7 @@ wb.doc.on( "mouseenter focusin", "[aria-haspopup] + [role=menu]", function( even
 wb.doc.on( "mouseleave", "[aria-haspopup]", function( event ) {
 
 	// There is no "mouseenter" in mobile
-	if ( docRootNode.className.indexOf( "smallview" ) === -1 ) {
+	if ( !isMobileMode ) {
 		CloseMenuWithDelay( event.currentTarget );
 	}
 } );
@@ -132,7 +132,7 @@ wb.doc.on( "mouseleave", "[aria-haspopup]", function( event ) {
 wb.doc.on( "focusout", "[aria-haspopup]", function( event ) {
 
 	// Don't close the submenu
-	if ( docRootNode.className.indexOf( "smallview" ) !== -1 ) {
+	if ( isMobileMode ) {
 		return;
 	}
 
@@ -152,7 +152,7 @@ wb.doc.on( "mouseleave focusout", "[aria-haspopup] + [role=menu]", function( eve
 	}
 
 	// There is no "mouseleave" in mobile
-	if ( docRootNode.className.indexOf( "smallview" ) !== -1 ) {
+	if ( isMobileMode ) {
 		return;
 	}
 
@@ -184,7 +184,7 @@ wb.doc.on( "click", "[aria-haspopup]", function( event ) {
 	var elm = event.currentTarget;
 
 	// Only for mobile view
-	if ( docRootNode.className.indexOf( "smallview" ) !== -1 ) {
+	if ( isMobileMode ) {
 
 		// Toggle
 		if ( elm.getAttribute( "aria-expanded" ) === "true" ) {
@@ -203,6 +203,8 @@ wb.doc.on( "click", "[aria-haspopup]", function( event ) {
 
 function CollapseThirdMenuLevel() {
 
+	isMobileMode = true;
+
 	// Expand the "most requested" link
 	var mnu3Level = document.querySelectorAll( "[role=menu] [role=menu] [role=menuitem][aria-haspopup=menu]" ),
 		i, i_len = mnu3Level.length;
@@ -213,6 +215,8 @@ function CollapseThirdMenuLevel() {
 }
 
 function ExpandThirdMenuLevel() {
+
+	isMobileMode = false;
 
 	// Expand the "most requested" link
 	var mnu3Level = document.querySelectorAll( "[role=menu] [role=menu] [role=menuitem][aria-haspopup=menu]" ),
@@ -227,7 +231,7 @@ wb.doc.on( "mediumview.wb largeview.wb xlargeview.wb", ExpandThirdMenuLevel );
 wb.doc.on( "smallview.wb xsmallview.wb", CollapseThirdMenuLevel );
 
 // If we are in mobile, collapse the third menu level
-if ( docRootNode.className.indexOf( "smallview" ) !== -1 ) {
+if ( isMobileMode ) {
 	CollapseThirdMenuLevel();
 }
 
@@ -236,7 +240,7 @@ if ( docRootNode.className.indexOf( "smallview" ) !== -1 ) {
 //
 
 // Make menu-item not tabbable
-$( ".gcweb-v2 [tabindex='-1']" ).addClass( "gcweb-v2-not-tabble-persist" );
+$( ".gcweb-v2 [data-keep-expanded=md-min]" ).addClass( "gcweb-v2-not-tabble-persist" );
 $( ".gcweb-v2 [role=menuitem]" ).attr( "tabindex", -1 );
 
 /**
@@ -289,29 +293,39 @@ wb.doc.on( "keydown", ".gcweb-v2 button, .gcweb-v2 [role=menuitem]", function( e
 		currentFocusIsOn = elm;
 	}
 
-	// NEXT MENU ITEM
-	var nextSiblingMenuItem;
-	if ( currentFocusIsOn.parentElement.nextElementSibling ) {
-		nextSiblingMenuItem = currentFocusIsOn.parentElement.nextElementSibling.querySelector( "[role=menuitem]:not(.gcweb-v2-not-tabble-persist)" );
-
-		// Check if we have hit a separator
-		if ( !nextSiblingMenuItem ) {
-			nextSiblingMenuItem = currentFocusIsOn.parentElement.nextElementSibling.nextElementSibling.querySelector( "[role=menuitem]:not(.gcweb-v2-not-tabble-persist)" );
-		}
-	} else {
-
-		// Get first item
-		if ( currentFocusIsOn.parentElement.parentElement.previousElementSibling.classList.contains( "gcweb-v2-not-tabble-persist" ) ) {
-			nextSiblingMenuItem = currentFocusIsOn.parentElement.parentElement.parentElement.parentElement.querySelector( "[role=menuitem]:not(.gcweb-v2-not-tabble-persist)" );
-		} else {
-			nextSiblingMenuItem = currentFocusIsOn.parentElement.parentElement.querySelector( "[role=menuitem]:not(.gcweb-v2-not-tabble-persist)" );
-		}
-	}
+	var isCurrentButtonMenu = ( currentFocusIsOn.nodeName === "BUTTON" );
 
 	// FIRST CHILD POPOP
 	var firstChildPopup;
 	if ( currentFocusIsOn.nextElementSibling ) {
-		firstChildPopup = currentFocusIsOn.nextElementSibling.querySelector( "[role='menuitem']:not(.gcweb-v2-not-tabble-persist)" );
+		firstChildPopup = currentFocusIsOn.nextElementSibling.querySelector( "[role='menuitem']" );
+	}
+
+	// NEXT MENU ITEM
+	var nextSiblingMenuItem;
+	if ( currentFocusIsOn.parentElement.nextElementSibling ) {
+		nextSiblingMenuItem = currentFocusIsOn.parentElement.nextElementSibling.querySelector( "[role=menuitem]" );
+
+		// Check if we have hit a separator, go to the next one. The separator can't be the last item and are not followed by another separator.
+		if ( !nextSiblingMenuItem ) {
+			nextSiblingMenuItem = currentFocusIsOn.parentElement.nextElementSibling.nextElementSibling.querySelector( "[role=menuitem]" );
+		}
+	} else {
+
+		// Get first item and take in consideration if the sub-menu is persistant open
+		if ( !isMobileMode && currentFocusIsOn.dataset.keepExpanded && firstChildPopup ) {
+
+			// The current focus is on a persistant open menu item, the next menu item is the first child
+			nextSiblingMenuItem = firstChildPopup;
+		} else if ( !isMobileMode && currentFocusIsOn.parentElement.parentElement.previousElementSibling.dataset.keepExpanded ) {
+
+			// The current focus is on the last item of the persistant menu
+			// Should go the next item of the parent menu item (not supported),
+			// but in our use case this is the first item of the parent men
+			nextSiblingMenuItem = currentFocusIsOn.parentElement.parentElement.parentElement.parentElement.querySelector( "[role=menuitem]" );
+		} else {
+			nextSiblingMenuItem = currentFocusIsOn.parentElement.parentElement.querySelector( "[role=menuitem]" );
+		}
 	}
 
 	// PARENT
@@ -320,56 +334,74 @@ wb.doc.on( "keydown", ".gcweb-v2 button, .gcweb-v2 [role=menuitem]", function( e
 	// PREVIOUS MENU ITEM
 	var previousSiblingMenuItem;
 	if ( currentFocusIsOn.parentElement.previousElementSibling ) {
-		previousSiblingMenuItem = currentFocusIsOn.parentElement.previousElementSibling.querySelector( "[role=menuitem]:not(.gcweb-v2-not-tabble-persist)" );
+		previousSiblingMenuItem = currentFocusIsOn.parentElement.previousElementSibling.querySelector( "[role=menuitem]" );
 
-		// Check if we have hit a separator
+		// Check if we have hit a separator. A separator is not the first items
 		if ( !previousSiblingMenuItem ) {
-			previousSiblingMenuItem = currentFocusIsOn.parentElement.previousElementSibling.previousElementSibling.querySelector( "[role=menuitem]:not(.gcweb-v2-not-tabble-persist)" );
+			previousSiblingMenuItem = currentFocusIsOn.parentElement.previousElementSibling.previousElementSibling.querySelector( "[role=menuitem]" );
 		}
-	} else if ( currentFocusIsOn.parentElement.parentElement.previousElementSibling.classList.contains( "gcweb-v2-not-tabble-persist" ) ) {
+	} else {
 
-		// Skip the separator
-		var children = currentFocusIsOn.parentElement.parentElement.parentElement.parentElement.children;
-		previousSiblingMenuItem = children[ children.length - 3 ].querySelector( "[role=menuitem]" );
+		// Get the last item, take in consideration one level of persistant open menu
+		if ( !isMobileMode && currentFocusIsOn.parentElement.parentElement.lastElementChild.querySelector( "[role=menuitem]" ).dataset.keepExpanded ) {
 
-	} else if ( currentFocusIsOn.nodeName !== "BUTTON" ) {
+			// The last item is persistant open, get it's last children
+			previousSiblingMenuItem = currentFocusIsOn.parentElement.parentElement.lastElementChild.querySelector( "[role=menuitem]" ).nextElementSibling.lastElementChild.querySelector( "[role=menuitem]" );
+		} else if ( !isMobileMode && currentFocusIsOn.parentElement.parentElement.previousElementSibling.dataset.keepExpanded && parentPopupBtn ) {
 
-		var children = currentFocusIsOn.parentElement.parentElement.children;
-		previousSiblingMenuItem = children[ children.length - 1 ].querySelector( "[role=menuitem]" );
+			// Get the parent, this is the first items of a persistant open menu
+			previousSiblingMenuItem = parentPopupBtn;
+		} else if ( isCurrentButtonMenu ) {
 
-		if ( previousSiblingMenuItem.classList.contains( "gcweb-v2-not-tabble-persist" ) ) {
-			children = previousSiblingMenuItem.nextElementSibling.children;
-			previousSiblingMenuItem = children[ children.length - 1 ].querySelector( "[role=menuitem]" );
+			// Get the last menu item
+			previousSiblingMenuItem = currentFocusIsOn.nextElementSibling.lastElementChild.querySelector( "[role=menuitem]" );
+		} else {
+
+			// Get the last item of the current menu
+			previousSiblingMenuItem = currentFocusIsOn.parentElement.parentElement.lastElementChild.querySelector( "[role=menuitem]" );
 		}
+
+	}
+
+	/*
+	 * Developer note:
+
+	For the "Most requested" menu.
+	- Don't skip that navigation item when it receive the focus
+	- when looking for the last, if that menuitem is expanded, then grab it's last items.
+	*/
+
+	if ( !isCurrentButtonMenu ) {
+		currentFocusIsOn.setAttribute( "tabindex", "-1" );
 	}
 
 	var elmToGiveFocus;
-
 	if ( key === "down" && nextSiblingMenuItem  ) {
 		elmToGiveFocus = nextSiblingMenuItem;
 	} else if ( key === "up" && previousSiblingMenuItem ) {
 		elmToGiveFocus = previousSiblingMenuItem;
-	} else if ( key === "right" && firstChildPopup || key === "enter" && firstChildPopup ) {
+	} else if ( ( !isCurrentButtonMenu && key === "right" && firstChildPopup ) || key === "enter" && firstChildPopup ) {
 		elmToGiveFocus = firstChildPopup;
-	} else if ( key === "left" ||  key === "esc" ) {
+	} else if ( ( !isCurrentButtonMenu && key === "left" ) || ( !isCurrentButtonMenu && key === "esc" ) ) {
 		elmToGiveFocus = parentPopupBtn;
 	} else if ( key  === "tab"  ) {
 		return;
 	}
 
-	if ( key === "left" ||  key === "esc" ) {
+	if ( !isCurrentButtonMenu && ( key === "left" ||  key === "esc" ) ) {
 
 		// Close the menu
-		if ( docRootNode.className.indexOf( "smallview" ) !== -1 &&
+		if ( isMobileMode &&
 				elmToGiveFocus.getAttribute( "aria-expanded" ) === "true" ) {
 			elmToGiveFocus.setAttribute( "aria-expanded", "false" );
 		}
 	}
 
-	// Focus on the element, check if visible before if we are on mobile
-
+	// Focus on the element
 	if ( elmToGiveFocus ) {
-		if ( docRootNode.className.indexOf( "smallview" ) !== -1 ) {
+
+		// Open the sub-menu automatically for mobile and menu button
+		if ( isMobileMode || isCurrentButtonMenu ) {
 			var popup = elmToGiveFocus.parentElement.parentElement.previousElementSibling;
 			if ( popup.getAttribute( "aria-expanded" ) !== "true" ) {
 
@@ -378,11 +410,12 @@ wb.doc.on( "keydown", ".gcweb-v2 button, .gcweb-v2 [role=menuitem]", function( e
 			}
 		}
 
+		elmToGiveFocus.setAttribute( "tabindex", "0" );
 		elmToGiveFocus.focus();
-	}
 
-	// Stop default behaviour
-	event.stopImmediatePropagation();
-	event.preventDefault();
+		// Stop default behaviour
+		event.stopImmediatePropagation();
+		event.preventDefault();
+	}
 
 } );
