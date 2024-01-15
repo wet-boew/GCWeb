4c392da4f6f487eb81a78914c6300129a37ec479
nfrenette <nick.frenette@pch.gc.ca>
2024-01-15 10:41:33 -0500

    Méli-mélo - 2023-09-menu - Update (#2308)
    Switched to using data-ajax-replace to fix CSS bug in Basic HTML mode

--- méli-mélo/2023-09-menu/campaign-menu.js
+++ méli-mélo/2023-09-menu/campaign-menu.js
@@ -5,12 +5,6 @@
 (function ($, window, document, wb) {
     "use strict";
 
-    /*
-     * Variable and function definitions.
-     * These are global to the plugin - meaning that they will be initialized once per page,
-     * not once per instance of plugin on the page. So, this is a good place to define
-     * variables that are common to all instances of the plugin on a page.
-     */
     var componentName = "campaign-menu",
         selector = "." + componentName + ".gcweb-menu",
         initEvent = "wb-init" + selector,
@@ -21,7 +15,6 @@
          * @param {jQuery Event} event Event that triggered the function call
          */
         init = function (event) {
-
             // Start initialization
             // returns DOM object = proceed with init
             // returns undefined = do not proceed with init (e.g., already initialized)
@@ -29,15 +22,7 @@
                 $elm;
 
             if (elm) {
-
-                // At this point, the GCWeb menu is already initialized
                 $elm = $(elm);
-
-                // Add IDs for GC menu. This must be synchronized with the ids for the mega menu below.
-                var gcSubmenus = $elm.find("ul ul");
-                for (var i = 1; i < gcSubmenus.length + 1; i++) {
-                    gcSubmenus.eq(i - 1).attr("id", 'sub-menu-' + i);
-                }
 
                 // Check if there is already a gcweb menu. 
                 // If there are 2 present, the global GCWeb menu is present, hide this custom menu
@@ -58,65 +43,38 @@
                     return;
                 }
 
-                // Megamenu does not exist, let's build it for md and lg using the content in the GCWeb
+                // Build megamenu once WET is fully initialized, to ensure the GCWeb menu ajax append is complete
+                $(document).on("wb-ready.wb", buildMegaMenu($elm));
+
+                wb.ready($elm, componentName);
+            }
+        },
+
+        buildMegaMenu = function ($elm) {
+            return function (e) {
                 // Retrieve the top level list items from GCWeb men
                 var gcwebMenuListItems = $elm.find("> ul > li");
 
                 // Start building mega menu
                 var megamenuHTML = "";
-                var listItemCounter = 0;
-                var subMenuCounter = 0;
                 $.each(gcwebMenuListItems, function (key, element) {
-                    // console.log(key + ": " + element);
-                    listItemCounter++;
-
                     // Get top level list item's anchor
-                    var listItemAnchor = element.querySelector("a");
-                    listItemAnchor.setAttribute('class', 'item');
-                    //console.log("anchor: ",  listItemAnchor);
-
-                    // Get top level list item's children
-                    var listItemchildren = element.querySelectorAll("li");
-                    //console.log("children: ",  listItemchildren);
-                    //console.log("children length: " + gcwebMenuListItems.length);                  
-
-                    // Build top level list items, with and without submenus                    
-                    if (listItemchildren.length > 0) {
-                        // Use counter to generate dynamic href bookmarks and ids for submenus
-                        subMenuCounter++;
-
-                        // Build list item with a submenu
-                        let subMenuId = "menu-" + subMenuCounter;
-
-                        var submegamenuHTML = "";
-                        // TODO: Verify all the attributes
-                        megamenuHTML += `
-                        <li><a href="#${subMenuId}" class="item" tabindex="0" aria-posinset="${listItemCounter}" aria-setsize="${gcwebMenuListItems.length}" role="menuitem" aria-haspopup="true">${listItemAnchor.textContent}</a>`;
-
-                        $.each(listItemchildren, function (key, element) {
-                            //console.log(key + ": " + element);
-                            var subListItemAnchor = element.querySelector("a");
-                            var href = subListItemAnchor.getAttribute('href');
-
-                            // TODO: Add missing attributes present in the 
-                            submegamenuHTML += `<li><a href="${href}">${subListItemAnchor.textContent}</a></li>`;
-                        });
-
-                        megamenuHTML += `<ul class="sm list-unstyled" id="${subMenuId}" role="menu">${submegamenuHTML}</ul></li>`;
-                    } else {
-                        // Build list item without a submenu
-                        let href = listItemAnchor.getAttribute('href');
-                        megamenuHTML += `<li><a href="${href}">${listItemAnchor.textContent}</a></li>`;
-                    }
+                    var anchor = element.querySelector("a");
+                    // Build list item without a submenu
+                    let href = anchor.getAttribute('href');
+                    let linkText = anchor.textContent;
+                    megamenuHTML += `<li><a href="${href}">${linkText}</a></li>`;
                 });
 
                 // Get GCWeb h2
                 var gcwebMenuH2 = document.querySelector(selector + " > h2");
 
+                var megamenuAjaxReplace = $elm[0].getAttribute('data-megamenu-ajax');
+
                 // Wrap menu HTML with the megamenu wrapper
                 // NOTE: Removed role="navigation" (redundant) and typeof="SiteNavigationElement" (not required)
                 megamenuHTML = `
-                <nav id="wb-sm" class="campaign-menu wb-menu visible-md visible-lg">
+                <nav id="wb-sm" class="campaign-menu wb-menu visible-md visible-lg" data-trgt="mb-pnl" data-ajax-replace="${megamenuAjaxReplace}">
                     <div class="pnl-strt nvbar">
                         <h2>${gcwebMenuH2.textContent}</h2>
                         <ul role="menubar" class="list-inline menu">
@@ -124,44 +82,32 @@
                         </ul>
                     </div>
                 </nav>`;
-                //console.log(megamenuHTML);                          
 
                 // Apply appropriate visible CSS classes to the GCWeb menu
                 $elm.addClass("visible-sm visible-xs");
 
+                // Workaround - Add the div required to build the hamburger menu, but hide it, since all we're trying to do is fully initialize the megamenu
+                $(".gcweb-menu").after('<div id="mb-pnl" hidden></div>');
+                $(".gcweb-menu").after('<div id="wb-glb-mn" hidden><h2> </h2></div>');
+
                 // Add the megamenu
                 $elm.after(megamenuHTML);
-
-                // Start the megamenu plugin
-                // Workaround - Add the div required to build the hamburger menu, but hide it, since all we're trying to do is fully initialize the megamenu
-                $(".wb-menu").attr('data-trgt', 'mb-pnl');
-                $(".wb-menu").after('<div id="mb-pnl" hidden></div>');
-                $(".wb-menu").after('<div id="wb-glb-mn" hidden><h2> </h2></div>');
 
                 // Initialize it
                 $(".wb-menu").trigger("wb-init.wb-menu");
 
-                $("#mb-pnl").remove();
-                $("#wb-glb-mn").remove();
-                $(".wb-menu").removeAttr('data-trgt');
-
-                wb.ready($elm, componentName);
+                $(document).on("wb-ready.wb-menu", cleanUp);
             }
         },
 
-        findNavElement = function (activeElement) {
-            let parentElement = activeElement.parentElement;
-            while (parentElement.nodeName != "NAV" && parentElement.nodeName != "BODY") {
-                parentElement = parentElement.parentElement;
-            }
-            if (parentElement.nodeName == "NAV") {
-                return parentElement;
-            } else {
-                return false;
-            }
+        cleanUp = function () {
+            $("#mb-pnl").remove();
+            $("#wb-glb-mn").remove();
         },
 
-        openMenu = function () {
+        switchMenu = function () {
+            // Show/hide the appropriate menu when the viewport changes between xs/sm and md/lg
+            // and transfer the active element/active link (if it's within the menu) to the visible menu
             if (savedAnchorFocus != undefined) {
                 let nav = findNavElement(savedAnchorFocus);
 
@@ -180,18 +126,30 @@
 
                         // Determine if the focus is on a link or an anchor to open a submenu
                         if (savedFocusHref.match("^#")) {
-                            let submenuId = savedFocusHref.substring(6);
-                            $(".gcweb-menu a[aria-controls='sub-menu-" + submenuId + "']").focus();
+                            let submenuId = savedFocusHref.substring(14);
+                            let equivalentElement = $(".gcweb-menu a[aria-controls='gc-sub-menu-" + submenuId + "']");
+                            if (equivalentElement != undefined) {
+                                equivalentElement.focus();
+                                if (equivalentElement[0].hasAttribute('aria-haspopup') && equivalentElement[0].getAttribute('aria-haspopup') == "true") {
+                                    equivalentElement.attr("aria-expanded", "true");
+                                }
+                            } else {
+                                console.warn("Unable to find equivalent link in GCWeb menu top level items");
+                            }
                         } else {
                             let gcMenuFocusAnchor = $('.gcweb-menu a[href="' + savedFocusHref + '"]');
                             let ulParents = $(gcMenuFocusAnchor).parents("ul");
                             if (ulParents.length > 1) {
                                 let submenu = ulParents.eq(0);
                                 submenu.parent()
-                                    .children('a.item')
+                                    .children('a')
                                     .attr("aria-expanded", "true");
                             }
-                            gcMenuFocusAnchor.focus();
+                            if (gcMenuFocusAnchor != undefined) {
+                                gcMenuFocusAnchor.focus();
+                            } else {
+                                console.warn("Unable to find equivalent link in GCWeb menu submenu items");
+                            }
                         }
                     } else if (nav.classList.contains('gcweb-menu') && $(".gcweb-menu").is(":hidden")) {
                         // Close submenus
@@ -203,8 +161,13 @@
                         // Set focus on submenu
                         if (savedFocusHref.match("^#")) {
                             let gcSubmenuFocus = $(savedAnchorFocus).attr("aria-controls");
-                            let submenuId = gcSubmenuFocus.substring(9);
-                            $(".wb-menu a[href='#menu-" + submenuId + "']").focus();
+                            let submenuId = gcSubmenuFocus.substring(12);
+                            let equivalentElement = $(".wb-menu a[href='#wet-sub-menu-" + submenuId + "']");
+                            if (equivalentElement != undefined) {
+                                equivalentElement.focus();
+                            } else {
+                                console.warn("Unable to find equivalent link in WET megamenu top level items");
+                            }
                         } else {
                             // Find if it is focused a submenu item
                             let megaMenuFocusAnchor = $('.wb-menu a[href="' + savedFocusHref + '"]');
@@ -218,21 +181,36 @@
                                 submenu.addClass('open');
                                 submenu.parent().addClass('active sm-open');
                             }
-                            megaMenuFocusAnchor.focus();
+                            if (megaMenuFocusAnchor != undefined) {
+                                megaMenuFocusAnchor.focus();
+                            } else {
+                                console.warn("Unable to find equivalent link in WET megamenu submenu items");
+                            }
                         }
                     }
                 }
             }
+        },
+
+        findNavElement = function (activeElement) {
+            let parentElement = activeElement.parentElement;
+            while (parentElement.nodeName != "NAV" && parentElement.nodeName != "BODY") {
+                parentElement = parentElement.parentElement;
+            }
+            if (parentElement.nodeName == "NAV") {
+                return parentElement;
+            } else {
+                return false;
+            }
         };
 
     $(window).on("resize", function () {
-        //console.log(document.activeElement);
         if (document.activeElement.nodeName == 'A') {
             savedAnchorFocus = document.activeElement;
         }
     });
 
-    $document.on(wb.resizeEvents, openMenu);
+    $document.on(wb.resizeEvents, switchMenu);
 
     // Bind the init event of the plugin
     $document.on("timerpoke.wb " + initEvent, selector, init);
