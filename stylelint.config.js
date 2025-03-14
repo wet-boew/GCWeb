615c85703f2272c3cde96a2aa7ddfd84e1ee94b0
Nick Schonning <nschonni@gmail.com>
2025-03-13 23:57:54 -0400

    fix: declaration-empty-line-before

--- stylelint.config.js
+++ stylelint.config.js
@@ -96,7 +96,6 @@ module.exports = {
 			rules: {
 				"at-rule-empty-line-before": null,
 				"declaration-block-no-redundant-longhand-properties": null,
-				"declaration-empty-line-before": null,
 				"hue-degree-notation": null,
 				"keyframes-name-pattern": null,
 				"no-descending-specificity": null, // Extremely slow
